b84b1191b33847515c823b965d861959af7488c7
Greg Rock <grock006@gmail.com>
2015-05-07 23:39:07 -0700

    added animate css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+// titled scroll effect
 $(document).ready(function(){
     $(".main").tiltedpage_scroll({
         angle: 30
     });
-});
 
 
+// title shine effect
 var shine = new Shine(document.getElementById('headline'));
 
 function handleMouseMove(event) {
@@ -15,13 +16,22 @@ function handleMouseMove(event) {
 
 window.addEventListener('mousemove', handleMouseMove, false);
 
-
+// zoom in on hover
 $("small").hover(function(){ 
+      $(this).addClass('dark');
       $(' .tps-section:nth-child(1n+1) .tps-wrapper').animate({backgroundSize: "1050px"}, 500, "linear");
   }, function(){
+      $(this).removeClass('dark');
        $(' .tps-section:nth-child(1n+1) .tps-wrapper').animate({backgroundSize: "1000px"}, 500, "linear");
   }
 );
 
+$("small a i").hover(function(){
+    $(this).addClass('animated infinite pulse');
+  }, function(){
+    $(this).removeClass('animated infinite pulse');
+  }
+)
 
-         
+
+});         
